cb225275070a6386a3a5c71759adf521f7f1fc5e
Kyu <heyimkyu@mailbox.org>
2025-03-24 00:30:53 +0100

    fixed ocr and made it better with grimshot and no tmp image

--- scripts/ocr.sh
+++ scripts/ocr.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-# Optional slurp arguments
-SLURP_ARGS="${1:-}"
-
-# Temporary file name
-TMP_IMG="tmp.png"
-
 # Select area with slurp and capture with grim
-grim -g "$(slurp $SLURP_ARGS)" "$TMP_IMG" && \
-tesseract -l eng "$TMP_IMG" - | wl-copy && \
+grimshot save area - | \
+tesseract -l eng - - | wl-copy && \
 
-
-if [ -f "${TMP_IMG}" ]; then
-    notify-send -a "Ax-Shell" -i "${full_path}" "OCR Sucess" "Text Copied to Clipboard"
-else
-    notify-send -a "Ax-Shell" "OCR Aborted"
-fi
-rm "$TMP_IMG"
+notify-send -a "Ax-Shell" -i "${full_path}" "OCR Sucess" "Text Copied to Clipboard"
