0bef3d2ef8fa1ba736d8c8bc27b95f45dea8ca20
Kyu <heyimkyu@mailbox.org>
2025-03-23 02:45:26 +0100

    linking .current.wall to examples if it doesn't exist on startup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import setproctitle
 import os
-import shutil
 from fabric import Application
 from fabric.utils import get_relative_path, exec_shell_command_async
 from modules.bar import Bar
@@ -7,26 +7,19 @@ from modules.sidebar import SideBar
 from modules.notch import Notch
 from modules.dock import Dock
 from modules.corners import Corners
-import setproctitle
 
 # Direct import of data module to avoid possible circular imports
-from config.data import APP_NAME, CACHE_DIR, CONFIG_FILE, APP_NAME_CAP
-from modules.bar import Bar
-from modules.corners import Corners
-from modules.dock import Dock
-from modules.notch import Notch
+from config.data import APP_NAME, CACHE_DIR, CONFIG_FILE, CURRENT_WIDTH, CURRENT_HEIGHT, APP_NAME_CAP
 
 fonts_updated_file = f"{CACHE_DIR}/fonts_updated"
 
 if __name__ == "__main__":
     setproctitle.setproctitle(APP_NAME)
 
-    if not os.path.isfile(CONFIG_FILE):
-        exec_shell_command_async(f"python {os.path.expanduser(f"~/.config/Ax-Shell/config/config.py")}")
-
-    current_wall = os.path.expanduser("~/.current.wall")
-    if not os.path.exists(current_wall):
-        shutil.copyfile(os.path.expanduser(f"~/.config/{APP_NAME_CAP}/assets/wallpapers_example/example-1.jpg"), os.path.expanduser(f"~/.current.wall"))
+    current_wallpaper = os.path.expanduser("~/.current.wall")
+    if not os.path.exists(current_wallpaper):
+        example_wallpaper = os.path.expanduser(f"~/.wallpapers/example-1.jpg")
+        os.symlink(example_wallpaper, current_wallpaper)
 
     corners = Corners()
     bar = Bar()
@@ -38,7 +31,6 @@ if __name__ == "__main__":
     app = Application(f"{APP_NAME}", bar, notch, dock)
 
     def set_css():
-        from config.data import CURRENT_WIDTH, CURRENT_HEIGHT
         app.set_stylesheet_from_file(
             get_relative_path("main.css"),
             exposed_functions={
@@ -46,6 +38,7 @@ if __name__ == "__main__":
                 "overview_height": lambda: f"min-height: {CURRENT_HEIGHT * 0.1 * 2 + 32 + 56}px;",
             },
         )
+
     app.set_css = set_css
 
     app.set_css()
