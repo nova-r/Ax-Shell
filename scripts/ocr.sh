6827f7d37d6fffb9a58297ce5f9fff623e5a98e3
Kyu <heyimkyu@mailbox.org>
2025-03-24 17:42:16 +0100

    using grimblast instead of grimshot for ocr

--- scripts/ocr.sh
+++ scripts/ocr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Select area with slurp and capture with grim
-grimshot save area - | \
+grimblast --freeze save area - | \
 tesseract -l eng - - | wl-copy && \
 
 notify-send -a "Ax-Shell" -i "${full_path}" "OCR Sucess" "Text Copied to Clipboard"
